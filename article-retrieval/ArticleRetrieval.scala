ec14926a1ace2c1db4a9a8b64f5316f006271300
Edoardo Pirovano <edoardo.pirovano@wadh.ox.ac.uk>
2015-04-30 14:25:30 +0100

    Run and display a sentiment analysis on articles as well.

--- article-retrieval/ArticleRetrieval.scala
+++ article-retrieval/ArticleRetrieval.scala
@@ -4,12 +4,39 @@ import org.joda.time.DateTime
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 import scala.concurrent.Await
+import com.alchemyapi.api.AlchemyAPI
+import com.alchemyapi.api._
+import org.xml.sax.SAXException;
+import org.w3c.dom.Document;
+import java.io._;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.xml.sax.SAXException;
 
 object ArticleRetrieval {
-  def main(args: Array[String]) = {
+	def main(args: Array[String]) = {
 		val client = new GuardianContentClient("5q4xyarnabx5jzxr92rch59v")
 		val searchQuery = SearchQuery().q("Google").pageSize(20) // Build a search query of 20 articles about Google
 		val response = Await.result(client.getResponse(searchQuery), Duration.Inf) // Wait for result to be returned
-		for (result <- response.results) println(result.webPublicationDate + " - " + result.webTitle) // Print titles of articles
+		val alchemyObj = AlchemyAPI.GetInstanceFromString("3b90bf78e6b1892aa154ce40a7393d420ea5ee55");
+		for (result <- response.results) {
+			println(result.webPublicationDate + " - " + result.webTitle)
+			println(getStringFromDocument(alchemyObj.URLGetTextSentiment(result.webUrl)));
+		}
+	}
+	private def getStringFromDocument(doc: Document): String = {
+		val domSource = new DOMSource(doc)
+		val writer = new StringWriter()
+		val result = new StreamResult(writer)
+		val tf = TransformerFactory.newInstance()
+		val transformer = tf.newTransformer()
+		transformer.transform(domSource, result)
+		writer.toString()
 	}
 }
