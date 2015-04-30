7d999951dcce48107dfbe26c19be846888741587
Edoardo Pirovano <edoardo.pirovano@wadh.ox.ac.uk>
2015-04-30 13:32:11 +0100

    Make an example for retrieving tweets.

---
+++ article-retrieval/TweetRetrieval.scala
@@ -0,0 +1,36 @@
+import twitter4j._
+import twitter4j.conf.ConfigurationBuilder
+import scala.collection.JavaConversions._
+ 
+object TweetRetrieval { 
+	val consumerKey = "zNlV1WdlTzhmeoRGGSuAUF0SI"
+	val consumerSecret = "5sWRHmv3HRsCpuI4VYNrVD0SH5GCuVTY4hUc9NEVw4dP5HyjWa"
+  def main(args : Array[String]) {
+		// Build a configuration to fetch the authentication token.
+		val builder = new ConfigurationBuilder()
+			.setApplicationOnlyAuthEnabled(true)
+      .setOAuthConsumerKey(consumerKey)
+      .setOAuthConsumerSecret(consumerSecret)
+    val token = new TwitterFactory(builder.build()).getInstance().getOAuth2Token()
+
+		// Build a configuration and corresponding Twitter object to use.
+    val cb = new ConfigurationBuilder()
+    cb.setDebugEnabled(true)
+			.setApplicationOnlyAuthEnabled(true)
+      .setOAuthConsumerKey(consumerKey)
+      .setOAuthConsumerSecret(consumerSecret)
+			.setOAuth2TokenType(token.getTokenType())
+			.setOAuth2AccessToken(token.getAccessToken())
+    val tf = new TwitterFactory(cb.build())
+    val twitter = tf.getInstance()
+ 
+    // Use the twitter object to fetch and print some tweets.
+		val query = new Query("Google")
+		query.setCount(20)
+    val result = twitter.search(query)
+    val tweets = result.getTweets();
+		for (tweet <- tweets) {
+				println("@" + tweet.getUser().getScreenName() + " - " + tweet.getText());
+		}
+  }
+}
