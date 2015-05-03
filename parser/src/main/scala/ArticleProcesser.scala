075189e1bf48c2aa52d413c75d547e2fb9af8a09
Jake Callaghan <jake.e.callaghan@gmail.com>
2015-05-03 19:44:13 +0100

    Update the ArticleProcessor to perform targeted API queries

--- parser/src/main/scala/ArticleProcesser.scala
+++ parser/src/main/scala/ArticleProcesser.scala
@@ -27,38 +27,27 @@ object ArticleProcesser {
 	val conf = ConfigFactory.load();	
 	val alchemyKey = conf.getString("apikeys.alchemy");
 	val alchemyObj = AlchemyAPI.GetInstanceFromString(alchemyKey);
-	/** an object to allow additional 'NamedEntity' parameters to be specified during API calls */
-	val entityParams : AlchemyAPI_NamedEntityParams = new AlchemyAPI_NamedEntityParams();
-	/** give a sentiment analysis for each entity found in an API query using entityParams */
-	entityParams.setSentiment(true);
+	// defines params
+	val sentimentParams = new AlchemyAPI_TargetedSentimentParams();
+	
 	def processArticle(url: String, stock: String): (Int, Int) = { // Importance, sentiment
-		val importance = SiteRank.getPopularity(url)
-		val alchemyResult = asXml(alchemyObj.URLGetRankedNamedEntities(url,entityParams));
-		println(alchemyResult);
-		val info = getCompanyEntities(alchemyResult);
-		for (entity <- info) {
-			if (entity.name contains stock) return (importance, Math.round((1+entity.sentiment).toFloat*50))
+		println("stock = "+stock+" in url = "+url);
+		val importance = SiteRank.getPopularity(url);
+		try {
+			val alchemyResult = asXml(alchemyObj.URLGetTargetedSentiment(url,stock,sentimentParams));
+			println(alchemyResult);
+			return (importance, Math.round((1+getSentimentScore(alchemyResult)).toFloat*50));
+		} catch {
+			case e : Exception => throw new IllegalArgumentException(stock+" couldn't be found in "+url);
 		}
-		return (0,0)
+
 	}
-	/** returns a sequence of CompanyEntity objects */
-	private def getCompanyEntities(file: Node) : List[CompanyEntity] = {
-		var companyEntities = List[CompanyEntity]();
-		// split the file, a node for each entity
-		val entities = file \\ "entity"; 
-		for (entity <- entities) {
-			// is this entity a company?
-			if ((entity \ "type")(0).text == "Company") {
-				val compEnt = new CompanyEntity();
-				compEnt.name = (entity \\ "text")(0).text;
-				compEnt.sentiment = (entity \\ "score")(0).text.toDouble;
-				compEnt.relevance = (entity \\ "relevance")(0).text.toDouble;
-				println(compEnt);
-				companyEntities = compEnt :: companyEntities;
-			}
-		} 
-		return companyEntities;
-	}
+	
+	/** finds the sentiment score within the XML document */
+	private def getSentimentScore(file: Node) : Double = {
+		return (file \\ "score")(0).text.toDouble;
+	} 
+	
 	private def asXml(dom: _root_.org.w3c.dom.Node): Node = {
 	  val source = new DOMSource(dom)
 	  val adapter = new NoBindingFactoryAdapter
