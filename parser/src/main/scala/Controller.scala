e865f0374fecfa085fc0bc78d7dbeef56fcaee06
Edoardo Pirovano <edoardo.pirovano@wadh.ox.ac.uk>
2015-05-03 19:38:42 +0100

    Add some error handling in controller.

--- parser/src/main/scala/Controller.scala
+++ parser/src/main/scala/Controller.scala
@@ -10,13 +10,20 @@ object Controller {
   def main(args : Array[String]) {
 		implicit val session = AutoSession
 		while(true) {
-			println("Beginning refresh of data.")
+			println("Beginning processing of articles.")
 			val stocks = List[(String,String)](("GOOG", "Google"))
 			for ((ticker, stockName) <- stocks) {
 				for (source <- sources) {
 					for ((date, title, url) <- source.getArticles(ticker, stockName)) {
-						val (importance, sentiment) = ArticleProcesser.processArticle(url, stockName)
-						sql"insert into articles(stock,date,source,title,importance,sentiment) values (${ticker}, ${date}, ${url}, ${title}, ${sentiment}, ${importance})".update.apply()
+						try {
+							val (importance, sentiment) = ArticleProcesser.processArticle(url, stockName)
+							sql"insert into articles(stock,date,source,title,importance,sentiment) values (${ticker}, ${date}, ${url}, ${title}, ${sentiment}, ${importance})".update.apply()
+							println("Article successfully added to database.")
+						} catch {
+							case e: IllegalArgumentException => {
+								println("Article could not be parsed to extract a sentiment score.")
+							}
+						}
 					}
 				}
 			}
