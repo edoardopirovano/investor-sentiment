a542fe23d0f6f5cc0be2a5dcfd0f4a0d239c43c4
Edoardo Pirovano <edoardo.pirovano@wadh.ox.ac.uk>
2015-05-03 23:43:03 +0100

    Remove unused file.

--- parser/src/main/scala/CompanyEntity.scala
+++
@@ -1,7 +0,0 @@
-/** a simple class to encapsulate an Entity and its data */
-class CompanyEntity {
-	var name : String = "DEFAULT";
-	var sentiment : Double = 0.0;
-	var relevance : Double = 0.0;
-	override def toString() : String = "(name = "+name+", sentiment = "+sentiment+", relevance = "+relevance+")"
-}
