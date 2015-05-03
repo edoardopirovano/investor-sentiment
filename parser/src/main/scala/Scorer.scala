ffd1ddf3a66c4096bf966082ed0e130026e439fe
Edoardo Pirovano <edoardo.pirovano@wadh.ox.ac.uk>
2015-05-03 17:36:50 +0100

    Factor out baseImportance into a configuration variable.

--- parser/src/main/scala/Scorer.scala
+++ parser/src/main/scala/Scorer.scala
@@ -1,8 +1,11 @@
 import scalikejdbc._
 import scalikejdbc.config._
 import org.joda.time._
- 
+import com.typesafe.config.ConfigFactory
+
 object Scorer {
+	val conf = ConfigFactory.load();
+	val baseImportance = conf.getInt("scorer.baseImportance");
 	
 	// Make Article class and companion object with constructor
 	case class Article(id: Long, date: DateTime, importance: Int, sentiment: Int)
@@ -28,8 +31,8 @@ object Scorer {
 					sentimenttotal = 0
 					volume = 0
 				}
-				volume += article.importance+10
-				sentimenttotal += (article.importance+10)*article.sentiment
+				volume += article.importance+baseImportance
+				sentimenttotal += (article.importance+baseImportance)*article.sentiment
 			}
 			val sentiment = sentimenttotal/volume
 			sql"insert into scores(stock,date,sentiment,volume) values (${stock}, ${date}, ${sentiment}, ${volume})".update.apply()
