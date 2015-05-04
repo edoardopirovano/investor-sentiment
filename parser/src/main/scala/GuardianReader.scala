8f9427dfde9185d8605cf6d9d3b7851bdd802127
Edoardo Pirovano <edoardo.pirovano@wadh.ox.ac.uk>
2015-05-04 17:12:52 +0100

    Factor out number of articles fetched using Guardian API to configuration file.

--- parser/src/main/scala/GuardianReader.scala
+++ parser/src/main/scala/GuardianReader.scala
@@ -10,11 +10,12 @@ object GuardianReader extends ArticleFetcher {
 	val conf = ConfigFactory.load();
 	val guardianKey = conf.getString("apikeys.guardian");
 	val alchemyKey = conf.getString("apikeys.alchemy");
+	val numArticles = conf.getInt("guardian.numArticles");
 	
 	def getArticles(tickerSymbol: String, stockName: String): List[Result] = {
 		var results = List[Result]()
 		val client = new GuardianContentClient(guardianKey)
-		val searchQuery = SearchQuery().q(stockName).pageSize(50) // Build a search query of 50 articles about stock
+		val searchQuery = SearchQuery().q(stockName).pageSize(numArticles) // Build a search query of 50 articles about stock
 		val response = Await.result(client.getResponse(searchQuery), Duration.Inf) // Wait for result to be returned
 		for (result <- response.results) {
 			results = (result.webPublicationDate, result.webTitle, result.webUrl) :: results
